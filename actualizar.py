f7c49a4b8e9524313287d2ddd326ffe316d0d166
oscarsotelo <oscaresotelo@gmail.com>
2023-07-13 09:56:46 -0300

    primerodeljueves

--- actualizar.py
+++ actualizar.py
@@ -1,10 +1,33 @@
-import sqlite3
 import streamlit as st
+import sqlite3
 import pandas as pd
 import base64
+
 # Conexión a la base de datos SQLite
 conn = sqlite3.connect('embargos.db')
 c = conn.cursor()
+
+# Función para buscar registros por DNI
+def buscar_por_dni(dni):
+    c.execute("SELECT * FROM Juicios WHERE Dni = ?", (dni,))
+    return c.fetchall()
+def buscar_por_id(id):
+    c.execute("SELECT * FROM Juicios WHERE id = ?", (id,))
+    return c.fetchall()
+
+
+# Función para actualizar un registro
+def actualizar_registro(id, campo, valor):
+    c.execute("UPDATE Juicios SET {} = ? WHERE ID = ?".format(campo), (valor, id))
+    conn.commit()
+    return c.rowcount
+
+if "id" not in st.session_state:
+    st.session_state.id = ""
+if "campo" not in st.session_state:
+    st.session_state.campo = ""
+
+# Título de la aplicación
 
 LOGO_IMAGE = "./imagenes/justice.png"
 
@@ -34,47 +57,49 @@ st.markdown(
     f"""
     <div class="container">
         <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
-        <h1 style='text-align: center;'>Juicios-Embargos</h1>
+        <p class="logo-text">Juicios-Embargos</p>
     </div>
     """,
     unsafe_allow_html=True
 )
 
+st.title("Actualizacion De Registros")
+# Entrada del número de DNI
+dni = st.sidebar.text_input("Ingrese el número de DNI")
 
-# Función para buscar juicios por DNI
-def buscar_por_dni(dni):
-    c.execute("SELECT * FROM Juicios WHERE Dni=?", (dni,))
-    return c.fetchall()
+# Botón para buscar el registro
+if st.sidebar.button("Buscar"):
+    resultados = buscar_por_dni(dni)
+    if len(resultados) > 0:
+        df = pd.DataFrame(resultados, columns=["id", "Dni", "NombreJuicio", "NumerodeExpte", "RadicacionJudicial",
+                                               "NumeroOficio", "NumeroExpteAdministrativo", "MedidaCautelar",
+                                               "AclaracionCautelar", "FechaInicio", "FechaFin", "Observacion",
+                                               "MontoEmbargo", "Sanciones", "NominacionRadicacion", "Juzgado"])
+        st.dataframe(df)
+    else:
+        st.write("No se encontraron registros con el DNI especificado.")
 
-# Función para actualizar un campo en un juicio
-def actualizar_campo(id_juicio, campo, valor):
-    c.execute(f"UPDATE Juicios SET {campo}=? WHERE id=?", (valor, id_juicio))
-    conn.commit()
-
-# Configuración de la aplicación Streamlit
-st.title('Modificacion Valores Juicios')
-
-# Control para buscar por DNI
-dni_busqueda = st.text_input('Buscar por DNI')
-if st.button('Buscar'):
-    juicios_encontrados = buscar_por_dni(dni_busqueda)
-    if juicios_encontrados:
-        # Convertir la lista de resultados a un DataFrame
-        df = pd.DataFrame(juicios_encontrados, columns=['ID', 'Dni', 'NombreJuicio', 'NumerodeExpte', 'RadicacionJudicial',
+# Sección para actualizar un campo del registro
+id = st.text_input("Ingrese el ID del registro que desea actualizar")
+campo = st.selectbox("Seleccione el campo a actualizar", ['NombreJuicio', 'NumerodeExpte', 'RadicacionJudicial',
                                                         'NumeroOficio', 'NumeroExpteAdministrativo', 'MedidaCautelar',
                                                         'AclaracionCautelar', 'FechaInicio', 'FechaFin', 'Observacion',
                                                         'Sanciones', 'MontoEmbargo','NominacionRadicacion', 'Juzgado'])
+valor = st.text_input("Ingrese el nuevo valor")
 
-        st.dataframe(df)
-        for juicio in juicios_encontrados:
-            st.write('---')
-            # Control para modificar cada campo del juicio
-            for i, campo in enumerate(juicio[2:], start=2):
-                nuevo_valor = st.text_input(f'{df.columns[i]}:', value=campo, key=f'campo_{i}')
-                if st.button(f'Actualizar {df.columns[i]}', key=f'boton_{i}'):  # Asignar claves únicas a los widgets
-                    actualizar_campo(juicio[0], df.columns[i], nuevo_valor)
+if st.button("Guardar"):
+    registros_actualizados = actualizar_registro(id, campo, valor)
+    ver_modificacion = buscar_por_id(id)
+    df = pd.DataFrame(ver_modificacion, columns=["id", "Dni", "NombreJuicio", "NumerodeExpte", "RadicacionJudicial",
+                                               "NumeroOficio", "NumeroExpteAdministrativo", "MedidaCautelar",
+                                               "AclaracionCautelar", "FechaInicio", "FechaFin", "Observacion",
+                                               "MontoEmbargo", "Sanciones", "NominacionRadicacion", "Juzgado"])
+    st.dataframe(df)
+
+    if registros_actualizados > 0:
+        st.info("Se actualizó correctamente el registro.")
     else:
-        st.write('No se encontraron juicios para el DNI especificado.')
+        st.warning("No se pudo actualizar el registro.")
 
 # Cierre de la conexión a la base de datos
 conn.close()
---
+++ grid.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import sqlite3
+from st_aggrid import AgGrid , GridUpdateMode
+
+from st_aggrid.grid_options_builder import GridOptionsBuilder 
+
+import pandas as pd
+
+# Conexión a la base de datos
+conn = sqlite3.connect('embargos.db')
+c = conn.cursor()
+
+# Función para buscar registros en la tabla Juicios por DNI
+@st.cache_data
+def buscar_juicios_por_dni(dni):
+    c.execute("SELECT * FROM Juicios WHERE Dni=?", (dni,))
+    juicios = c.fetchall()
+    return(juicios)
+    # df = pd.DataFrame(juicios, columns=["id", "Dni", "NombreJuicio", "NumerodeExpte", "RadicacionJudicial", "NumeroOficio", "NumeroExpteAdministrativo", "MedidaCautelar", "AclaracionCautelar", "FechaInicio", "FechaFin", "Observacion", "MontoEmbargo", "Sanciones", "NominacionRadicacion", "Juzgado"])
+    # gd = GridOptionsBuilder.from_dataframe(df)
+    # gd.configure_pagination(enabled=True)
+    # gd.configure_default_column(editable=True, groupable=True)
+    # sel_mode = st.radio("Seleccione ", options = ["uno", "multiples"])
+    # gd.configure_selection(selection_mode = sel_mode ,use_checkbox=True)
+    # gridoptions = gd.build() 
+    # AgGrid(df, gridOptions=gridoptions)
+
+# Título de la aplicación
+st.title('Búsqueda de Juicios por DNI')
+
+# Formulario para ingresar el DNI
+dni = st.text_input('Ingrese el DNI')
+
+# Botón para realizar la búsqueda
+if st.button('Buscar'):
+    if dni:
+        # Realizar la búsqueda en la base de datos
+        if "juicios" not in st.session_state:
+            st.session_state.juicios = ""
+        juicios = buscar_juicios_por_dni(dni)
+        df = pd.DataFrame(juicios, columns=["id", "Dni", "NombreJuicio", "NumerodeExpte", "RadicacionJudicial", "NumeroOficio", "NumeroExpteAdministrativo", "MedidaCautelar", "AclaracionCautelar", "FechaInicio", "FechaFin", "Observacion", "MontoEmbargo", "Sanciones", "NominacionRadicacion", "Juzgado"])
+        st.session_state.juicios = df
+        
+gd = GridOptionsBuilder.from_dataframe(st.session_state.juicios)
+gd.configure_pagination(enabled=True)
+gd.configure_default_column(editable=True, groupable=True)
+sel_mode = st.radio("Seleccione ", options = ["uno", "multiples"])
+gd.configure_selection(selection_mode = sel_mode ,use_checkbox=True)
+gridoptions = gd.build() 
+grid_table = AgGrid(st.session_state.juicios, gridOptions=gridoptions,
+                    update_mode = GridUpdateMode.SELECTION_CHANGED,
+                    height = 500,
+                    allow_unsafe_jscode = True,
+                    
+                    )
+sel_row = grid_table["selected_rows"]
+st.write(sel_row)
+
+        # Mostrar los resultados en pantalla
+    #     if juicios:
+    #         st.write('Juicios encontrados:')
+           
+    #     else:
+    #         st.write('No se encontraron juicios para el DNI ingresado.')
+    # else:
+    #     st.write('Por favor, ingrese un DNI válido.')
+
+# Cerrar la conexión a la base de datos
+conn.close()
